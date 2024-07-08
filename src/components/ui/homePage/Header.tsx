fa8e5793e34803028a7a46d6aef78bcd0548d451
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-07-08 23:20:13 +0600

    header section implemented

--- src/components/ui/homePage/Header.tsx
+++ src/components/ui/homePage/Header.tsx
@@ -1,8 +1,115 @@
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
-    <div className="flex justify-between items-center">
-      <h1>Hello</h1>
-      <h1>Gello</h1>
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a>
+                <NavLink to="/">Home</NavLink>
+              </a>
+            </li>
+            <li>
+              <a>
+                <NavLink to="/products">Products</NavLink>
+              </a>
+            </li>
+            <li>
+              <a>
+                <NavLink to="/products/management">Management</NavLink>
+              </a>
+            </li>
+            <li>
+              <a>
+                <NavLink to="/products/details">Details</NavLink>
+              </a>
+            </li>
+            <li>
+              <a>
+                <NavLink to="/products/cart">Cart</NavLink>
+              </a>
+            </li>
+            <li>
+              <a>
+                <NavLink to="/products/checkout">Checkout</NavLink>
+              </a>
+            </li>
+            <li>
+              <a>
+                <NavLink to="/about">About Us</NavLink>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <img
+          className="size-7 text-primary"
+          src="../../../../public/facicon1.png"
+          alt=""
+        />
+        <a className="text-xl font-bold ml-2">
+          Fitness <span className="text-blue-500">H</span>ub
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex navbar-end">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>
+              <NavLink to="/">Home</NavLink>
+            </a>
+          </li>
+          <li>
+            <a>
+              <NavLink to="/products">Products</NavLink>
+            </a>
+          </li>
+          <li>
+            <a>
+              <NavLink to="/products/management">Management</NavLink>
+            </a>
+          </li>
+          <li>
+            <a>
+              <NavLink to="/products/details">Details</NavLink>
+            </a>
+          </li>
+          <li>
+            <a>
+              <NavLink to="/products/cart">Cart</NavLink>
+            </a>
+          </li>
+          <li>
+            <a>
+              <NavLink to="/products/checkout">Checkout</NavLink>
+            </a>
+          </li>
+          <li>
+            <a>
+              <NavLink to="/about">About Us</NavLink>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
