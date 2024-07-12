143942aef23c3c43905c3c685c04b5da0667faa1
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-07-12 20:31:52 +0600

    prevent reload page with warning message using react-beforeunload

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import MainLayout from "./components/layouts/MainLayout";
+import { useBeforeunload } from "react-beforeunload";
+import { useAppSelector } from "./redux/hooks";
+import { RootState } from "./redux/store";
 
 function App() {
+  const cart = useAppSelector((state: RootState) => state.cart.items);
+  useBeforeunload((event) => {
+    if (cart.length > 0) {
+      event.preventDefault();
+    }
+  });
   return (
     <>
       <MainLayout></MainLayout>
