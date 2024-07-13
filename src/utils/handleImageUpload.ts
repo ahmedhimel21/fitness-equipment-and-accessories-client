a91ed5c08ed07e2f7dd82ada2170ab7abe05867b
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-07-13 17:08:13 +0600

    some issue fixed

--- src/utils/handleImageUpload.ts
+++ src/utils/handleImageUpload.ts
@@ -1,7 +1,5 @@
 const handleImageUpload = async (file: string | Blob) => {
-  // console.log(process.env.VITE_IMGBB_API_KEY);
   const secretKey = import.meta.env.VITE_IMGBB_KEY;
-  console.log(secretKey);
 
   const formData = new FormData();
   formData.append("image", file);
